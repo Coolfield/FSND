2dad1f776d46bbc94bcaae2511054684b379bff3
Coolfield <Coolfield@users.noreply.github.com>
2020-06-11 22:33:31 +0100

    Fyyur Add missing venue objects

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -34,16 +34,16 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    #genres = db.Column(db.ARRAY(db.String))
+    genres = db.Column(db.ARRAY(db.String))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    #website = db.Column(db.String(120))
+    website = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    #seeking_talent = db.Column(db.Boolean())
-    #seeking_description = db.Column(db.String(500))
+    seeking_talent = db.Column(db.Boolean())
+    seeking_description = db.Column(db.String(500))
 
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
